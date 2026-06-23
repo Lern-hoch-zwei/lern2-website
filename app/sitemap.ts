65994bde7e3478388cffb46ec69e11400ad9fa72
Lern-hoch-zwei <info@lern2.com>
2026-06-23 16:30:42 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,13 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const base = 'https://www.lern2.com'
   return [
-    { url: 'https://lern2.com', lastModified: new Date() },
-    { url: 'https://lern2.com/ar', lastModified: new Date() },
-    { url: 'https://lern2.com/tr', lastModified: new Date() },
-    { url: 'https://lern2.com/fa', lastModified: new Date() },
-    { url: 'https://lern2.com/impressum', lastModified: new Date() },
-    { url: 'https://lern2.com/datenschutz', lastModified: new Date() },
+    { url: `${base}`, lastModified: new Date() },
+    { url: `${base}/ar`, lastModified: new Date() },
+    { url: `${base}/tr`, lastModified: new Date() },
+    { url: `${base}/fa`, lastModified: new Date() },
+    { url: `${base}/impressum`, lastModified: new Date() },
+    { url: `${base}/datenschutz`, lastModified: new Date() },
   ]
 }
